7dcb1d0c644900396613990fe7c4bc5a79dfe8a0
prafulpolimera <praful.polimera@gmail.com>
2020-02-20 14:14:39 +0530

    Webdriver Resources

---
+++ automation/seleniumResources/userActionsWrapper/DriverActionsWrapper.java
@@ -0,0 +1,14 @@
+package userActionsWrapper;
+
+
+/*
+ * Contains wrappers for the Selenium Actions that can be used directly in the test cases. 
+ * These wrappers avoid updating the test cases when newer versions of the dependencies are available.
+ * This would help by just updating the wrappers where the actual Selenium Actions are created
+ */
+
+public class DriverActionsWrapper {
+	
+	
+
+}
